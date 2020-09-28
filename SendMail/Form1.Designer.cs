da9d30b2c73a4055fbf5fdfd19028ddf9a6597e3
Katsee <ghtjd000129@naver.com>
2020-09-28 11:31:41 +0900

    첫 커밋

---
+++ SendMail/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SendMail
+{
+    partial class SendMail
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImportButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.DataTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Location = new System.Drawing.Point(594, 12);
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Size = new System.Drawing.Size(94, 36);
+            this.ImportButton.TabIndex = 0;
+            this.ImportButton.Text = "불러오기";
+            this.ImportButton.UseVisualStyleBackColor = true;
+            this.ImportButton.Click += new System.EventHandler(this.ImportButton_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(694, 12);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(94, 36);
+            this.SendButton.TabIndex = 1;
+            this.SendButton.Text = "전송";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // DataTable
+            // 
+            this.DataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataTable.Location = new System.Drawing.Point(12, 54);
+            this.DataTable.Name = "DataTable";
+            this.DataTable.RowTemplate.Height = 23;
+            this.DataTable.Size = new System.Drawing.Size(775, 384);
+            this.DataTable.TabIndex = 2;
+            this.DataTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataTable_CellContentClick);
+            // 
+            // SendMail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataTable);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.ImportButton);
+            this.Name = "SendMail";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ImportButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.DataGridView DataTable;
+    }
+}
+
---
+++ SendMail/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendMail
+{
+    public partial class SendMail : Form
+    {
+        public SendMail()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    public class CSVParser
+    {
+        private static char[] delimiters = { ',', ';' };            //구분자
+
+        private static string[] keys = { "address", "file" };       
+        public static string[] Keys { get { return keys; } }
+
+        public static Dictionary<string, string> Parse(string file)
+        {
+            Dictionary<string, string> parsedData = new Dictionary<string, string>();   //key : string / value : List<string>
+
+            string[] lines = file.Split('\n');
+            string[] splitedLine;
+
+            foreach (var line in lines)
+            {
+                splitedLine = line.Split(delimiters);
+
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    parsedData.Add(keys[i], splitedLine[i]);
+                }
+            }
+
+            return parsedData;
+        }
+    }
+}
