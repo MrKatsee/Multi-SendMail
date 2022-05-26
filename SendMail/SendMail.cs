c2d15bd38d074480f0f13b26753e68e0db7cc15c
LAPTOP-P47PDQP9\HS <aaiiiho@gmail.com>
2022-05-26 22:45:35 +0900

    설치 추가 + 쓸모없는 참조 제거

--- SendMail/SendMail.cs
+++ SendMail/SendMail.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Mail;
-using System.Runtime.InteropServices;
 using System.IO;
-using System.Reflection;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
 using PdfSharp.Pdf.Security;
